8add183c0855a32481854a36fda8e15463ba476a
unknown <kuruvamadhu993@gmail.com>
2023-10-15 21:07:29 +0530

    first

---
+++ app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def wish(request):
+    d={'name':'Nikky','age':10}
+    return render(request,'wish.html',context=d)
+
+
+def conditions(request):
+    d={'a':24400,'b':3600,'c':400}
+    return render(request,'conditions.html',context=d)
+
+def loop(request):
+    d={'name':'ASHU'}
+    return render(request,'loop.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
